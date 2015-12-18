9b35b37f3f4752295beaf3e479a56fa1696f7733
hessproject <hessn1@duq.edu>
2015-12-18 12:55:48 -0500

    Code cleanup and input validation

--- js/index.js
+++ js/index.js
@@ -3,41 +3,37 @@ var app = angular.module('calcApp',[]);
 app.controller('calcController', ['$scope',function($scope){
   //set default display
   $scope.display = "--";
-  //check if last button hit was equals
-  $scope.hitEquals = false;
   $scope.buttonPress = function(btnStr){
     var lastChar = $scope.display.charAt($scope.display.length-1)
-    //if display is blank or the last button hit was equals, start new display
-    if ($scope.display==="--" || $scope.hitEquals){
-      $scope.hitEquals = false;
+    //if display is blank, start new sdisplay
+    if ($scope.display=== "--"){
       if ("*/0".indexOf(btnStr) === -1){
         $scope.display = btnStr;
-      } else {
-        $scope.display = $scope.display;
       }
-    } else if ("+-/*.".indexOf(lastChar) != -1 && "+/*-".indexOf(btnStr) != -1 || lastChar =="." && btnStr == "."){
+    } else if (("+-/*.".indexOf(lastChar) != -1 && "+/*-".indexOf(btnStr) != -1) || (lastChar == "." && btnStr == ".")){
       $scope.display = $scope.display;
     }
     else {
       $scope.display = $scope.display.concat(btnStr);
     }
   };
+
   $scope.equals = function(){
     var lastChar = $scope.display.charAt($scope.display.length-1)
-    $scope.hitEquals = true;
-    //if last button entered before equals was a function, remove it and still evaluate
+    //if last button entered before equals was a function button, remove it and still evaluate
     if ("+-/*.".indexOf(lastChar) != -1){
       $scope.display = $scope.display.substr(0,$scope.display.length-1)
     }
     var decimals = document.getElementById("decimalplaces").value;
+    $scope.display.replace(/[^-()\d/*+.]/g, ''); //sanitize string for use in eval() function
     $scope.display = eval($scope.display).toFixed(decimals).toString();
   };
+
   $scope.clear = function(){
-    $scope.hitEquals = false;
     $scope.display = "--";
   };
+
   $scope.backspace = function (){
-    $scope.hitEquals = false;
     var length = $scope.display.length
     if (length > 1 && $scope.display !== "--"){
       $scope.display = $scope.display.substring(0, length - 1)
